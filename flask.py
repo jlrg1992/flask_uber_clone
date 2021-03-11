3da7658e5a89e5ab2369ea8b488c5a3406f07ae2
Jorge L. Rangel García <jlrg1992@gmail.com>
2021-03-10 21:30:12 -0600

    assets working

---
+++ flask.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Buajajaja'
+
+@app.route('/app/')
+def home():
+    return render_template('index.html')
+@app.route('/assets/<path>')
+def get_assets(path)
+	return render_template(f'assets/{path}')
